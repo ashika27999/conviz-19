3f012e491a0305efd71df7e8410412d65ab7f07b
ashika27999 <59637194+ashika27999@users.noreply.github.com>
2020-07-26 22:46:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import HomePage from "./pages/homePage";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
